aa97ffe6230282f66b323e29079d448d6baf6ea9
aeliyadevs <aeliyadevs@gmail.com>
2024-07-11 23:46:00 +0545

    integrate share on facebook, twitter and linkedIn options for sharing posts

--- src/components/SocialShare.tsx
+++ src/components/SocialShare.tsx
@@ -2,16 +2,22 @@ import { Link } from "react-router-dom";
 import ShareIcon from "./ul/ShareIcon";
 
 const SocialShare = () => {
+  const link =
+    "https://www.setopati.com/literature/333543?fbclid=IwZXh0bgNhZW0CMTEAAR1aWNEOStOBdaq6cgoMqyfqGyoytweHvjjOhUr66MRqdvgFzxjJB6JI6P0_aem_3HyPBOT_ZYa-uB2NWSd6vw";
+  const message = "Sharing the article";
+  const fbShareLink = `https://www.facebook.com/sharer/sharer.php?u=${link}`;
+  const twitterShareLink = `https://www.twitter.com/share?&url=${link}&text=${message}&hashtags=javascipt,typescript,tailwind,web-development`;
+  const linkedinShareLink = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
   return (
     <div className="flex gap-4 items-center my-6">
       Share:
-      <Link to="">
+      <Link to={fbShareLink} target="_blank">
         <ShareIcon icon={<i className="fa-brands fa-facebook-f"></i>} />
       </Link>
-      <Link to="">
+      <Link to={twitterShareLink} target="_blank">
         <ShareIcon icon={<i className="fa-brands fa-twitter"></i>} />
       </Link>
-      <Link to="">
+      <Link to={linkedinShareLink} target="_blank">
         <ShareIcon icon={<i className="fa-brands fa-linkedin-in"></i>} />
       </Link>
     </div>
