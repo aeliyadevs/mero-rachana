1e948174705cf3dddcb96eee87e8546f0309b6c7
aeliyadevs <aeliyadevs@gmail.com>
2024-07-21 22:19:22 +0545

    add new property 'rating' to the book reviews and display stars on single page

--- src/pages/Authors/Profile.tsx
+++ src/pages/Authors/Profile.tsx
@@ -3,6 +3,7 @@ import { getAuthorById } from "../../utils/GetData";
 import FeaturedCard from "../../components/FeaturedCard";
 import HeadingTwo from "../../components/ul/HeadingTwo";
 import { posts } from "../../data/defaultPosts.json";
+import CardAlt from "../../components/CardAlt";
 
 const Profile = () => {
   const { id } = useParams();
@@ -11,7 +12,7 @@ const Profile = () => {
     (post) => post.author === parseInt(id ? id : "0")
   );
   return (
-    <div className="w-7/12 mx-auto my-16">
+    <div className="w-6/12 mx-auto my-16">
       <div className="w-full">
         <img
           src={author?.profileImage}
@@ -31,32 +32,32 @@ const Profile = () => {
           <p>
             <strong>3k</strong> followers | <strong>300</strong> posts
           </p>
-          <p className="pl-4 my-3 border-l-2 border-lime-400">
+          <p className="pl-4 my-3 border-l-4 border-sky-200">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
             explicabo velit, quaerat harum magni eveniet ullam quod corporis
             neque deserunt voluptate aperiam voluptatibus quas saepe voluptates?
             Modi nihil odio eum id pariatur! Sit, fugit tenetur.
           </p>
           <div className="flex gap-4 pt-2">
-            <div className="w-8 h-8 rounded-full bg-slate-600 text-white flex justify-center items-center">
+            <div className="w-8 h-8 rounded-full bg-sky-400 text-white flex justify-center items-center">
               <i className="fa-brands fa-facebook-f"></i>
             </div>
-            <div className="w-8 h-8 rounded-full bg-slate-600 text-white flex justify-center items-center">
+            <div className="w-8 h-8 rounded-full bg-sky-400 text-white flex justify-center items-center">
               <i className="fa-brands fa-instagram"></i>
             </div>
-            <div className="w-8 h-8 rounded-full bg-slate-600 text-white flex justify-center items-center">
+            <div className="w-8 h-8 rounded-full bg-sky-400 text-white flex justify-center items-center">
               <i className="fa-brands fa-x-twitter"></i>
             </div>
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-6">
-        <div className="col-span-3">
-          <HeadingTwo heading={"All posts by " + author?.name} />
+      <div className="my-10">
+        <HeadingTwo heading={author?.name + "'s post"} />
+        <div className="grid grid-cols-2 gap-6">
+          {postsByAuthorId.map((post) => (
+            <CardAlt id={post.postId} />
+          ))}
         </div>
-        {postsByAuthorId.map((post) => (
-          <FeaturedCard id={post.postId} />
-        ))}
       </div>
     </div>
   );
--- src/pages/Reviews/Review.tsx
+++ src/pages/Reviews/Review.tsx
@@ -3,23 +3,44 @@ import { getBookReviewById } from "../../utils/GetData";
 
 const Review = () => {
   const { id } = useParams();
-  console.log(id);
   const bookReview = getBookReviewById(parseInt(id ? id : "0"));
+  const renderStars = (count: number) => {
+    const stars = [];
+    // set rated stars
+    for (let i = 0; i < count; i++) {
+      stars.push(<i className="fa-solid fa-star"></i>);
+    }
+    // set unrated stars
+    for (let i = 0; i < 5 - count; i++) {
+      stars.push(<i className="fa-regular fa-star"></i>);
+    }
+    return stars;
+  };
+  if (!bookReview) {
+    return <>Review not found.</>;
+  }
   return (
-    <div className="w-7/12 mx-auto my-16 grid grid-cols-3 gap-6">
+    <div className="w-6/12 mx-auto my-16 grid grid-cols-3 gap-6">
       <div className="rounded-md overflow-hidden">
-        <img src={bookReview?.bookCover} alt={bookReview?.bookName} />
+        <img src={bookReview.bookCover} alt={bookReview.bookName} />
       </div>
       <div className="col-span-2 py-6">
-        <h1 className="text-5xl font-bold mb-3">{bookReview?.bookName}</h1>
+        <h1 className="text-5xl font-bold mb-3">{bookReview.bookName}</h1>
         <p>
-          Book by <strong>{bookReview?.author}</strong>
+          Book by <strong>{bookReview.author}</strong>
         </p>
         <p>
-          Published Year - <strong>{bookReview?.publishedYear}</strong>
+          Published Year - <strong>{bookReview.publishedYear}</strong>
         </p>
-        <blockquote className="mt-4 border-l-2 border-lime-400 pl-4">
-          {bookReview?.review}
+        <div className="rating text-xl text-sky-500">
+          {bookReview.rating ? (
+            renderStars(bookReview.rating)
+          ) : (
+            <span className="text-gray-400 text-base">N/A</span>
+          )}
+        </div>
+        <blockquote className="mt-4 border-l-4 border-sky-200 pl-4">
+          {bookReview.review}
         </blockquote>
       </div>
     </div>
