fb6284e92e38bd37d24dbf23af8b0d82696befd3
aeliyadevs <aeliyadevs@gmail.com>
2024-09-03 23:59:19 +0545

    add basic login form in the login component

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom";
-import Register from "./Register";
+import { Input, Submit, Textarea } from "../../components/form";
+import HeadingOne from "../../components/ui/HeadingOne";
 
 const Login = () => {
   return (
-    <div className="w-9/12 mx-auto text-center">
-      Login Page
+    <div className="w-6/12 mx-auto my-16">
+      <HeadingOne heading={"User Login"} center={true} />
+      <form action="" className="max-w-2xl mx-auto flex flex-col gap-3">
+        {/* <div className="flex gap-5"> */}
+        <Input
+          type="text"
+          name="username"
+          placeholder="Username"
+          required={true}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required={true}
+        />
+        {/* </div> */}
+        <Submit value="Login" center={true} />
+      </form>
       <div>
         Don't have an account?{" "}
         <Link to="/register" className="underline text-blue-500">
